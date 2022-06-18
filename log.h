c84759a8329ac103782267287d4925ce594fd4cb
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2022-06-18 05:58:35 -0300

    Improve log API

--- log.h
+++ log.h
@@ -2,10 +2,22 @@
 #define LOG_H
 
 #include <stdio.h>
+#include <apr_pools.h>
 
-FILE *log_open( const char *filename );
-void log_close( FILE *output );
-void log_print( FILE *output, const char *format, ... );
+typedef enum
+{
+    LOG_TYPE_INFO,
+    LOG_TYPE_WARN,
+    LOG_TYPE_ERROR,
+    LOG_TYPE_BLOCK,
+} log_type_t;
+
+struct log;
+typedef struct log log_t;
+
+log_t *log_open( const char *path, apr_pool_t *pool );
+void log_close( log_t *log );
+void log_print( log_t *log, log_type_t type, const char *format, ... );
 void syslog_print( const char *server, const char *format, ... );
 
 #endif // LOG_H
